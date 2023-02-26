da3d7b8879affb2557f7bc44cebc610010b1095e
Jose Cardoso <65740649+asininemonkey@users.noreply.github.com>
2023-02-26 21:08:40 +0000

    Create vmware.nix

---
+++ vmware.nix
@@ -0,0 +1,60 @@
+{ config, pkgs, ... }:
+
+{
+  environment = {
+    etc.vmware-tools.source = "${pkgs.open-vm-tools}/etc/vmware-tools/*";
+
+    systemPackages = [
+      pkgs.open-vm-tools
+    ];
+  };
+
+  home-manager.users.jcardoso = {
+    home.file = {
+      ".config/gtk-3.0/bookmarks".text = ''
+        file:///mnt/hgfs VMware Shared Folders
+      '';
+    };
+  };
+
+  security.wrappers.vmware-user-suid-wrapper = {
+    group = "root";
+    owner = "root";
+    setuid = true;
+    source = "${pkgs.open-vm-tools}/bin/vmware-user-suid-wrapper";
+  };
+
+  services.udev.packages = [
+    pkgs.open-vm-tools
+  ];
+
+  systemd.mounts = [
+    {
+      description = "VMware vmblock Fuse Mount";
+      options = "subtype=vmware-vmblock,default_permissions,allow_other";
+      type = "fuse";
+      unitConfig.ConditionVirtualization = "vmware";
+
+      wantedBy = [
+        "multi-user.target"
+      ];
+
+      what = "${pkgs.open-vm-tools}/bin/vmware-vmblock-fuse";
+      where = "/run/vmblock-fuse";
+    }
+  ];
+
+  systemd.services.vmware = {
+    after = [
+      "display-manager.service"
+    ];
+
+    description = "VMware Tools Daemon";
+    serviceConfig.ExecStart = "${pkgs.open-vm-tools}/bin/vmtoolsd";
+    unitConfig.ConditionVirtualization = "vmware";
+
+    wantedBy = [
+      "multi-user.target"
+    ];
+  };
+}
