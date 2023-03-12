3fe1ef2fe350d096553904556d89fcb66f634246
Jose Cardoso <65740649+asininemonkey@users.noreply.github.com>
2023-03-12 16:52:54 +0000

    Update Configuration

---
+++ scripts/repository-reset.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env zsh
+
+rm --force --recursive .git
+
+git init --initial-branch 'main'
+git add .
+git commit --message 'Repository Reset'
+git remote add origin 'git@github.com:asininemonkey/nixos.git'
+git push --force --mirror
+git push --set-upstream 'origin' 'main'
