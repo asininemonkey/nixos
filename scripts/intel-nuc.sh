ad6fe73f8f7399f8cb3b76818e9c3195977eca22
Jose Cardoso <65740649+asininemonkey@users.noreply.github.com>
2023-03-12 18:33:48 +0000

    Update Configuration

--- scripts/intel-nuc.sh
+++ scripts/intel-nuc.sh
@@ -2,7 +2,7 @@
 
 STORAGE='/dev/disk/by-id/nvme-eui.e8238fa6bf530001001b444a4658cbbe'
 
-sudo nvme format --force --ses 1 "${STORAGE}"
+# sudo nvme format --force --ses 1 "${STORAGE}"
 
 sudo parted --script "${STORAGE}" -- mklabel gpt
 sudo parted --script "${STORAGE}" -- mkpart esp fat32 8MB 1032MB
@@ -21,13 +21,15 @@ sudo mount '/dev/disk/by-label/nixos' '/mnt'
 
 sudo rm --force --recursive '/mnt/lost+found'
 
-sudo mkdir '/mnt/boot' '/mnt/data' '/mnt/etc'
+sudo mkdir --parents '/mnt/boot' '/mnt/data' '/mnt/etc/nixos'
 
 sudo mount '/dev/disk/by-label/boot' '/mnt/boot'
 
 sudo mount '/dev/disk/by-id/wwn-0x5002538840044dc7-part1' '/mnt/data'
 
 curl --location --silent 'https://raw.githubusercontent.com/asininemonkey/nixos/main/devices/intel-nuc.nix' | sudo tee '/mnt/etc/nixos/configuration.nix' > '/dev/null'
+
+nixos-generate-config --show-hardware-config --root '/mnt' | sudo tee '/mnt/etc/nixos/hardware-configuration.nix' > '/dev/null'
 
 sudo nixos-install --no-root-password
 
