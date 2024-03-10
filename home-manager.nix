2b6e903d19907e38e3e889a0fd2bf50dcfa78e2f
Jose Cardoso <65740649+asininemonkey@users.noreply.github.com>
2024-03-10 17:32:22 +0000

    Repository Update

--- home-manager.nix
+++ home-manager.nix
@@ -982,7 +982,7 @@ in
           nso = "nix --extra-experimental-features nix-command store optimise";
           ping = "trip";
           speedtest = "speedtest --secure --share";
-          tf = "terraform fmt --recursive";
+          tff = "terraform fmt --recursive";
           tmf = "terramate fmt";
           tmg = "terramate generate";
           top = "btop";
