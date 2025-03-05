9d95b02016470ef424649783883eb44e7721754a
Jose Cardoso <65740649+asininemonkey@users.noreply.github.com>
2025-03-05 10:44:47 +0000

    WIP

--- home-manager.nix
+++ home-manager.nix
@@ -734,6 +734,28 @@ in
         enable = true;
 
         extraConfig = {
+          # https://blog.gitbutler.com/how-git-core-devs-configure-git/
+          branch.sort = "-committerdate";
+          column.ui = "auto";
+          commit.verbose = true;
+          diff.algorithm = "histogram";
+          diff.colorMoved = "plain";
+          diff.mnemonicPrefix = true;
+          diff.renames = true;
+          fetch.all = true;
+          fetch.prune = true;
+          fetch.pruneTags = true;
+          help.autocorrect = "prompt";
+          push.autoSetupRemote = true;
+          push.default = "simple";
+          push.followTags = true;
+          rebase.autoSquash = true;
+          rebase.autoStash = true;
+          rebase.updateRefs = true;
+          rerere.autoupdate = true;
+          rerere.enabled = true;
+          tag.sort = "version:refname";
+
           commit.gpgsign = true;
           core.editor = "zeditor --wait";
           credential.helper = "libsecret";
