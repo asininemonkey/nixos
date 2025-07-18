3675ef1ddcc8cee4a45e0768542377a03c0a8d63
Jose Cardoso <65740649+asininemonkey@users.noreply.github.com>
2025-07-18 14:23:09 +0100

    WIP

--- home-manager/k9s.nix
+++ home-manager/k9s.nix
@@ -1,4 +1,32 @@
 {pkgs, ...}: {
+  home.file.".config/k9s/plugins.yaml".text = ''
+    plugins:
+      # https://github.com/derailed/k9s/blob/master/plugins/debug-container.yaml
+      debug:
+        args:
+          - -c
+          - "kubectl debug --image nicolaka/netshoot:v0.13 --kubeconfig ''${KUBECONFIG} --namespace ''${NAMESPACE} ''${POD} --share-processes --stdin --target ''${NAME} --tty -- bash"
+        background: false
+        command: bash
+        confirm: true
+        dangerous: true
+        description: Add debug container
+        scopes:
+          - containers
+        shortCut: Shift-D
+      # https://github.com/derailed/k9s/blob/master/plugins/dive.yaml
+      dive:
+        args:
+          - ''${COL-IMAGE}
+        background: false
+        command: dive
+        confirm: false
+        description: Dive image
+        scopes:
+          - containers
+        shortCut: d
+  '';
+
   programs.k9s = {
     enable = true;
 
